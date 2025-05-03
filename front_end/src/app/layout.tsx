3b28ca055fec8109f22015b18c6e98024bf9b0eb
HUNG NGO <hungngo1607@gmail.com>
2025-05-03 18:12:42 -0400

    fix again some

--- front_end/src/app/layout.tsx
+++ front_end/src/app/layout.tsx
@@ -1,11 +1,10 @@
 "use client";
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 import "@progress/kendo-theme-default/dist/all.css";
 import { SessionProvider } from "next-auth/react";
-import { Toaster } from "@/components/ui/sonner";
+import { Geist, Geist_Mono } from "next/font/google";
 import { Suspense } from "react";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
