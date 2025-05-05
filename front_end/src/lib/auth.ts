fd6fd8d7734001c9f82030e4474f0d6af7f9f3e7
HUNG NGO <hungngo1607@gmail.com>
2025-05-05 18:51:50 -0400

    finally done

--- front_end/src/lib/auth.ts
+++ front_end/src/lib/auth.ts
@@ -1,18 +1,14 @@
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/lib/prisma";
 import type { Adapter } from "next-auth/adapters";
 
-const prisma = new PrismaClient();
-
 export const authConfig = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
-    // ...add more providers here
   ],
   session: {
     strategy: "jwt",
