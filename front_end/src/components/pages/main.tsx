4604b2fe87541dbb5080cbac268089bca67157c2
HUNG NGO <hungngo1607@gmail.com>
2025-05-06 01:52:08 -0400

    add

--- front_end/src/components/pages/main.tsx
+++ front_end/src/components/pages/main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import { Model, models, types } from "@/components/data/models";
 import { presets } from "@/components/data/presets";
