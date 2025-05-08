b49fef1ddeebed5f5dd4ac742136d9781b4a9596
HUNG NGO <hungngo1607@gmail.com>
2025-05-07 21:03:40 -0400

    adding home page and vortex

--- front_end/src/app/page.tsx
+++ front_end/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { Vortex } from "@/components/ui/vortex";
+import GoogleSignIn from "@/components/auth/GoogleSignin";
 
 export default function Home() {
   return (
@@ -16,14 +17,11 @@ export default function Home() {
           Prompt Plus
         </h2>
         <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-          This is chemical burn. It&apos;ll hurt more than you&apos;ve ever been
-          burned and you&apos;ll have a scar.
+          Improve your prompt in a systematic way. Get the best out of your
+          idea!!
         </p>
         <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
-          <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
-            Order now
-          </button>
-          <button className="px-4 py-2  text-white ">Watch trailer</button>
+          <GoogleSignIn />
         </div>
       </Vortex>
     </div>
